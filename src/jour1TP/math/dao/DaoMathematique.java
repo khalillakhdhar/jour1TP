884c0b78a9a11ca6daec336cba2edbb526d6b228
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-10 10:25:34 +0100

    Initial commit

---
+++ src/jour1TP/math/dao/DaoMathematique.java
@@ -0,0 +1,10 @@
+package jour1TP.math.dao;
+
+public interface DaoMathematique {
+public int somme(int x,int y);
+public double produit(int a,int b);
+public double division(int a,int b);
+public int soustraction(int x,int y);
+public double factoriel(int c);
+
+}
